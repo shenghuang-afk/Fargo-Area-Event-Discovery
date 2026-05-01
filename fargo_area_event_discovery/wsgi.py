acfca70c7ac58c5369049da954f0a62328f49db5
Evanf15 <evan.fretheim@ndus.edu>
2026-05-01 15:14:10 -0500

    addition to html

---
+++ fargo_area_event_discovery/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fargo_area_event_discovery project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fargo_area_event_discovery.settings')
+
+application = get_wsgi_application()
