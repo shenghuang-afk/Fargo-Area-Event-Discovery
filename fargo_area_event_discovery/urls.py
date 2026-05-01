7066449de9715af96dbb0f594f24a8e6f2744433
shenghuang-afk <sheng.huang@ndsu.edu>
2026-05-01 16:08:50 -0500

    Added migrate

--- fargo_area_event_discovery/urls.py
+++ fargo_area_event_discovery/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('website.urls')),
     path('add-event/', views.add_event, name='add_event'),
     path('update-event/<int:event_id>/', views.update_event, name='update_event'),
+    
 ]
--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('event/<int:event_id>/status/<str:status>/', views.update_event_status, name='update_event_status'),
+    
 ]
